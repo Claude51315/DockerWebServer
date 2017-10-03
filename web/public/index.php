0ff63e0eea6fa1496e2fea692238c95d71e09c9e
claude51315 <claude51315@gmail.com>
2017-10-03 15:07:57 +0800

    add simple login interface

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,38 @@
 <?php
-
-include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
-
+/*
+	try {
+		$dsn = 'mysql:host=mysql; dbname=test;charset=utf8; port=3306';
+		$pdo = new PDO($dsn, 'dev', 'dev');
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+	}
+	$sqlState = "SELECT * from test1";
+	$re = $pdo->query($sqlState);
+	if($re != FALSE) {
+		echo "query sucess;\n";
+		foreach( $re as $row)
+		print $row['id'] . "\t" ;
+		print $row['name'] . "\t" ;
+		print $row['height'] . "\t" ;
+		print $row['weight'] . "\n";
+	} else {
+		echo "query fail;";
+	}
+*/
 ?><!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
+        <title>Simple Login</title>
     </head>
     <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
+        <h1>This is a simple login interface powered by Docker.</h1>
+		<p>Based on this <a href = "https://github.com/nanoninja/docker-nginx-php-mysql">Git Repo</a>, a web server environment can be easily established. </p>
     </body>
+	<form action="login.php" method="POST">
+		<div>  <p>Account = <input type="text" name="account"></p> </div>
+		<div>  <p>Password = <input type="password" name="password"></p> </div>
+		<div>  <input type="submit"> </div>
+	</form>
+		
 </html>
---
+++ web/public/login.php
@@ -0,0 +1,49 @@
+<?php
+	function connectDB(){
+		$dsn = 'mysql:host=mysql; dbname=test; charset=utf8; port=3306';
+		try {
+			$pdo = new PDO($dsn, 'dev', 'dev');
+		} catch (PDOException $e){
+			echo $e->getMessage();
+			return FALSE;
+		}
+		return $pdo;
+	}
+	function login($pdo, $account, $password){
+		$sql = sprintf("Select * from user where account = '%s' and password = '%s'",
+						$account, 
+						$password);
+		$re = $pdo->query($sql);
+		if($re == FALSE)
+			return FALSE;
+		else 
+			return TRUE;
+	}
+?>
+
+<?php
+	
+	$account = $_POST['account'];
+	$password = $_POST['password'];
+	$userTable=connectDB();
+	if($userTable != FALSE) {
+	
+	$ret = login($userTable, $account, $password);			
+		if($ret == TRUE){
+			echo "login success!";
+		} else {
+			echo "login fail!";
+		}
+	
+	} else {
+		echo "connect error!";
+	}
+
+
+
+
+
+
+
+
+?>
