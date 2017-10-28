a9d576fac31661a9e4d086d34c4fd5e29882262f
claude51315 <claude51315@gmail.com>
2017-10-28 09:29:40 +0800

    submit gift form with ajax

--- web/public/index.php
+++ web/public/index.php
@@ -1,40 +1,39 @@
-<?php
-require("func.php")
-/*
-	try {
-		$dsn = 'mysql:host=mysql; dbname=test;charset=utf8; port=3306';
-		$pdo = new PDO($dsn, 'dev', 'dev');
-	} catch (PDOException $e) {
-		echo $e->getMessage();
-	}
-	$sqlState = "SELECT * from test1";
-	$re = $pdo->query($sqlState);
-	if($re != FALSE) {
-		echo "query sucess;\n";
-		foreach( $re as $row)
-		print $row['id'] . "\t" ;
-		print $row['name'] . "\t" ;
-		print $row['height'] . "\t" ;
-		print $row['weight'] . "\n";
-	} else {
-		echo "query fail;";
-	}
-*/
-?><!DOCTYPE html>
+<?php require("func.php")?>
+
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <title>Xmas Gift Exchange</title>
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script>
+        $(function () {
+        $("#gift").submit( function (event) {
+            event.preventDefault();// using this page stop being refreshing 
+            $.ajax({
+            type: 'POST',
+            url: 'gift.php',
+            data: $('form').serialize(),
+            success: function () {
+                location.reload();
+                //alert('form was submitted');
+            }
+          });
+
+        });
+      });
+    </script>
     </head>
     <body>
         <h1>Chrismax Party with gift exchange!</h1>
         <p> Please provide the following information</p>    
-    <form action="gift.php" method="POST">
+    <!-- <form action="gift.php" method="POST"> -->
+    <form id="gift">
         <div> <p> Name = <input type="text" name = "name"></p></div>
         <div> <p> Adj1 = <input type="text" name = "adj1"></p></div>
         <div> <p> Adj2 = <input type="text" name = "adj2"></p></div>
         <div> <p> email = <input type="text" name = "email"></p></div>
-        <div> <input type="submit"></div>
+        <div> <input type="submit" value="submit"></div>
     </form>		
     <?php
         $ret=connectDB();
